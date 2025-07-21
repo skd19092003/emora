504b79008d4817820c49e8b1247ef47c618de991
skd19092003 <skd19092003@gmail.com>
2025-07-21 14:06:01 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" href="/logo.png" />
+        <link rel="icon" type="image/png" href="/logo.png"  />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -36,4 +36,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+} 
--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,35 @@
+
+import Link from "next/link";
+import { BookHeart } from 'lucide-react';
+// bg-background/95
 export default function Header() {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className="w-full fixed top-0 z-50  backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <div className="absolute inset-0 border-b border-primary/10" />
+            <header className="relative max-w-full mx-auto px-4 bg-amber-500 flex justify-between h-20 ">
+
+                {/* Left: Logo */}
+                
+                <Link
+                    href="/"
+                    className="flex gap-x-2 text-[23px] items-center p-0 m-0 bg-blue-500"
+                >
+                    
+                    <BookHeart size={40} />
+                    Emora-Ai
+                   
+              
+                </Link>
+                
+                {/* Right: Placeholder for buttons */}
+                <div className="flex items-center space-x-4 p-2 bg-amber-900">
+                    {/* Guest Access, Login, Signup buttons will go here */}
+                   <p className="bg-blue-500">nodnv</p> 
+                   <p className="bg-blue-500">nodnv</p> 
+                   <p className="bg-blue-500">nodnv</p> 
+                   
+                </div>
+            </header>
         </div>
     );
 }
